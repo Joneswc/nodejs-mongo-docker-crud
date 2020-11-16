6c188f6c81821dde567789e811a6f59a6d91974b
Jones Willians das Chagas <jones.chagas@inspira.br>
2020-11-16 15:05:55 -0300

    first complete sample

---
+++ README.md
@@ -0,0 +1,7 @@
+https://medium.com/@lameckanao/fazendo-um-crud-com-node-js-mongodb-e-docker-70ee6c8da8ca
+https://medium.com/@pedrompinto/criar-uma-app-crud-com-node-js-mongodb-9cdc703000ae
+https://medium.com/zenofai/how-to-build-a-node-js-and-mongodb-application-with-docker-containers-15e535baabf5
+
+
+https://itnext.io/dockerize-a-node-js-app-connected-to-mongodb-64fdeca94797
+https://github.com/vguleaev/Express-Mongo-Docker-tutorial/tree/master/test-mongo-app
---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// Adicionando arquivo de rota no endpoint /carros
+const carros = require('./routes/carros');
+
+app.use('/api/carros', carros);
+// adicionado
+
+mongoose
+  .connect('mongodb://db:27017/demo', {
+    useNewUrlParser: true
+  })
+  .then(result => {
+    console.log('MongoDB Conectado');
+  })
+  .catch(error => {
+    console.log("error error");
+    console.log(error);
+  });
+
+app.listen(9000, () => console.log('Server ativo na porta 9000'));
